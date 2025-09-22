ad8464f98d1daf79f5bb33dd242766546d9a789a
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-09-22 16:05:51 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import LandingPage from './pages/LandingPage';
+import './App.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <>
+     <MantineProvider>
+      <LandingPage />
+     </MantineProvider>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,16 @@
+import { AppShell } from "@mantine/core"
+
+export default function LandingPage() {
+  return(
+    <div>
+      <AppShell padding={'md'} header={{height: 50}}>
+        <AppShell.Header style={{backgroundColor: '#C6D870'}}>
+
+        </AppShell.Header>
+        <AppShell.Main>
+
+        </AppShell.Main>
+      </AppShell>
+    </div>
+  )
+}
